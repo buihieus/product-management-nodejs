ec257acd3f9f7ecf032848be8bd0f4960c8cb5ca
Văn Chí Khanh <vanchikhanh4@gmail.com>
2024-05-27 19:15:11 +0700

    Làm trang danh sách sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,13 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model")
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    });
+    console.log(products);
     res.render('client/pages/product/index', {
-        pageTitle: "Trang sản phẩm"
+        pageTitle: "Trang sản phẩm",
+        products: products
     });
 }
