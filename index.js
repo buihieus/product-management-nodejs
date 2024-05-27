abc168a7013aa86328c5d77383c2a80d1e682398
Văn Chí Khanh <vanchikhanh4@gmail.com>
2024-05-27 17:55:40 +0700

    Cài đặt Mongoose

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 
+
 require('dotenv').config()
 
 const routes = require("./routes/client/index.route")
 const app = express()
 const port = process.env.PORT; 
+
+const database = require("./config/database");
+database.connect();
 
 app.set('views', './views');
 app.set('view engine', 'pug');
