0f3d39585c6f6f6632b096402ba0424846bc1ad7
Văn Chí Khanh <vanchikhanh4@gmail.com>
2024-06-01 14:20:05 +0700

    Bộ lọc và tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,12 +1,50 @@
 const Product = require("../../models/product.model");
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-    const products = await Product.find({
+    console.log(req.query.status);
+    let filterStatus = [
+        {
+            name: "Tất cả",
+            status: "",
+            class: ""
+        },
+        {
+            name: "Hoạt động",
+            status: "active",
+            class: ""
+        },
+        {
+            name: "Dừng hoạt động",
+            status: "inactive",
+            class: ""
+        }
+    ]
+    if (req.query.status) {
+        const index = filterStatus.findIndex(item => item.status === req.query.status);
+        filterStatus[index].class = "active";
+    } else {
+        const index = filterStatus.findIndex(item => item.status == "");
+        filterStatus[index].class = "active";
+    }
+    let find = {
         deleted: false
-    });
-    console.log(products);
+    };
+    if (req.query.status)  {
+        find.status = req.query.status;
+    }
+    let keywords = "";
+    if (req.query.keyword) {
+        keywords = req.query.keyword;
+
+        const regex = new RegExp(keywords, "i");//regex tìm kiếm ko phần biệt hoa thường và theo chữ cái
+        find.title = regex;
+    }
+    const products = await Product.find(find);
+    // console.log(products);
     res.render('admin/pages/product/index', {
         pageTitle: "Trang sản phẩm",
-        products: products
+        products: products,
+        filterStatus: filterStatus,
+        keywords: keywords
     });
 };
---
+++ public/admin/js/script.js
@@ -0,0 +1,39 @@
+/*Button status*/ 
+const buttonsStatus = document.querySelectorAll("[button-status]");
+console.log(buttonsStatus);
+if (buttonsStatus.length > 0) {
+    let url = new URL(window.location.href);
+    buttonsStatus.forEach(button => {
+        button.addEventListener("click", () => {
+            const status = button.getAttribute("button-status");
+            console.log(status);
+
+            if (status) {
+                url.searchParams.set("status", status);
+            } else {
+                url.searchParams.delete("status");
+            }
+            window.location.href = url.href;
+        });
+    });
+    
+}
+/*End Button status*/ 
+
+//Form Search
+const formSearch = document.querySelector("#form-search");
+if (formSearch) {
+    let url = new URL(window.location.href);
+
+    formSearch.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const keyword = event.target.elements.keyword.value;
+        console.log(event.target.elements.keyword.value);
+        if (keyword) {
+            url.searchParams.set("keyword", keyword);
+        } else {
+            url.searchParams.delete("keyword");
+        }
+        window.location.href = url.href;
+    });
+}
