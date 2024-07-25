75bdcafc99ca010d4015c459929ee71cdf0b5592
Văn Chí Khanh <vanchikhanh4@gmail.com>
2024-07-25 13:50:09 +0700

    Trang chi tiết sản phẩm client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -5,9 +5,30 @@ module.exports.index = async (req, res) => {
     const products = await Product.find({
         status: "active",
         deleted: false
-    }).sort({ position : "asc"});
+    }).sort({ position : "desc"});
     res.render('client/pages/product/index', {
         pageTitle: "Trang sản phẩm",
         products: products
     });
+}
+
+// [GET] /products/:slug
+module.exports.detail = async (req, res) => {
+    try {
+        const find = {
+            deleted: false,
+            slug: req.params.slug,
+            status: "active"
+        };
+        const product = await Product.findOne(find);
+
+        res.render('client/pages/product/detail', {
+            pageTitle: product.title,
+            product: product
+        });
+        
+    } catch (error) {
+        res.redirect(`/products`);
+    }
+    
 }
--- routes/client/home.route.js
+++ routes/client/home.route.js
@@ -5,5 +5,4 @@ const controller = require("../../controllers/client/home.controller");
 
 router.get('/', controller.index)
 
-
 module.exports = router;//sử dụng trong file index.route
