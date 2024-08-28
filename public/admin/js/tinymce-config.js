b6ef62411261f5ac37eec5205e5df402948ce9bc
Văn Chí Khanh <vanchikhanh4@gmail.com>
2024-08-28 15:41:46 +0700

    Upload image

--- public/admin/js/tinymce-config.js
+++ public/admin/js/tinymce-config.js
@@ -1,35 +1,19 @@
 tinymce.init({
   selector: "textarea",
   plugins: "image",
-  /* enable title field in the Image dialog*/
-  image_title: true,
-  /* enable automatic uploads of images represented by blob or data URIs*/
-  automatic_uploads: true,
-  /*
-    URL of our upload handler (for more details check: https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_url)
-    images_upload_url: 'postAcceptor.php',
-    here we add custom filepicker only to Image dialog
-  */
-  file_picker_types: "image",
-  /* and here's our custom image picker*/
   file_picker_callback: (cb, value, meta) => {
-    const input = document.createElement("input");
-    input.setAttribute("type", "file");
-    input.setAttribute("accept", "image/*");
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
 
-    input.addEventListener("change", (e) => {
+    input.addEventListener('change', (e) => {
       const file = e.target.files[0];
 
       const reader = new FileReader();
-      reader.addEventListener("load", () => {
-        /*
-          Note: Now we need to register the blob in TinyMCEs image blob
-          registry. In the next release this part hopefully won't be
-          necessary, as we are looking to handle it internally.
-        */
-        const id = "blobid" + new Date().getTime();
-        const blobCache = tinymce.activeEditor.editorUpload.blobCache;
-        const base64 = reader.result.split(",")[1];
+      reader.addEventListener('load', () => {
+        const id = 'blobid' + (new Date()).getTime();
+        const blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+        const base64 = reader.result.split(',')[1];
         const blobInfo = blobCache.create(id, file, base64);
         blobCache.add(blobInfo);
 
@@ -41,6 +25,5 @@ tinymce.init({
 
     input.click();
   },
-  content_style:
-    "body { font-family:Helvetica,Arial,sans-serif; font-size:16px }",
+  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
 });
