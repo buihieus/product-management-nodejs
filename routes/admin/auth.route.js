e4e691d809326ff77dc720fa4c81c15fed09cb38
Văn Chí Khanh <vanchikhanh4@gmail.com>
2024-10-18 00:06:12 +0700

    Đăng xuất

--- routes/admin/auth.route.js
+++ routes/admin/auth.route.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
+const authValidate = require("../../validates/auth.validate");
 const controller = require("../../controllers/admin/auth.controller");
 
 router.get("/login", controller.login);
 
-router.post("/login", controller.loginPost);
+router.post("/login", authValidate.loginPost,controller.loginPost);
+
+router.get("/logout", controller.logout);
 
 module.exports = router;
