a8ff2131cac1fd73f4159ad36e9b4033c0ee3dbf
Bui Cong Hieu <buihieu9259@gmail.com>
2024-12-22 23:14:33 +0700

    test

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -2,104 +2,88 @@ const Account = require("../../models/account.model");
 const Role = require("../../models/role.model");
 const systemConfig = require("../../config/system");
 const md5 = require("md5");
-//Chịu trách nhiệm xử lý logic,Kết nối giữa Model và View,Xử lý trung gian
-// [GET] /admin/accounts
+// [GET] admin/accounts
 module.exports.index = async (req, res) => {
   let find = {
     deleted: false,
   };
   const records = await Account.find(find).select("-password -token");
-
   for (const record of records) {
     const role = await Role.findOne({
-      _id: record.role_id,
       deleted: false,
+      _id: record.role_id,
     });
     record.role = role;
   }
-
-  res.render("admin/pages/accounts/index.pug", {
-    pageTitle: "Danh sách tài khoản",
+  res.render("admin/pages/account/index", {
+    titlePage: "Danh sách tài khoản",
     records: records,
   });
 };
-
-// [GET] /admin/accounts/create
+// [GET] admin/accounts/create
 module.exports.create = async (req, res) => {
-  const roles = await Role.find({ deleted: false });
-  res.render("admin/pages/accounts/create.pug", {
-    pageTitle: "Tạo mới tài khoản",
-    roles: roles,
+  const record = await Role.find({
+    deleted: false,
+  });
+  res.render("admin/pages/account/create", {
+    titlePage: "Danh sách tài khoản",
+    roles: record,
   });
 };
-
-// [POST] /admin/accounts/create
+// [POST] admin/accounts/create
 module.exports.createPost = async (req, res) => {
-  const emailExist = await Account.findOne({
+  const emailExists = await Account.findOne({
     email: req.body.email,
     deleted: false,
   });
-
-  if (emailExist) {
-    req.flash("error", `Email ${req.body.email} đã tồn tại!`);
+  if (emailExists) {
+    req.flash("error", `Email ${req.body.email} đã tồn tại`);
     res.redirect("back");
   } else {
     req.body.password = md5(req.body.password);
-
-    const record = new Account(req.body);
-    await record.save();
-
+    const account = new Account(req.body);
+    await account.save();
     res.redirect(`${systemConfig.prefixAdmin}/accounts`);
   }
 };
-
-// [GET] /admin/accounts/edit/:id
+// [GET] admin/accounts/edit/:id
 module.exports.edit = async (req, res) => {
-  let find = {
-    _id: req.params.id,
-    deleted: false,
-  };
-
   try {
-    const data = await Account.findOne(find);
-    const roles = await Role.find({ deleted: false });
-
-    res.render("admin/pages/accounts/edit", {
-      pageTitle: "Chỉnh sửa tài khoản",
-      data: data,
+    const record = await Account.findOne({
+      deleted: false,
+      _id: req.params.id,
+    });
+    const roles = await Role.find({
+      deleted: false,
+    });
+    res.render("admin/pages/account/edit", {
+      titlePage: "Chỉnh sửa tài khoản",
       roles: roles,
+      record: record,
     });
   } catch (error) {
-    //
-    console.log(error);
-    req.flash("error", "Chỉnh sửa tài khoản thất bại!");
-    res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
+    res.redirect(`${systemConfig.prefixAdmin}/accounts`);
   }
 };
-
-// [PATCH] /admin/accounts/edit/:id
+// [PATCH] admin/accounts/edit/:id
 module.exports.editPatch = async (req, res) => {
   const id = req.params.id;
-  const emailExist = await Account.findOne({
-    _id: { $ne: id},
+  const emailExists = await Account.findOne({
+    _id: {$ne: id},
     email: req.body.email,
     deleted: false,
   });
-  try {
-    if (emailExist) {
-      req.flash("error", `Email ${req.body.email} đã tồn tại!`);
+  if (emailExists) {
+    req.flash("error", `Email ${req.body.email} đã tồn tại`);
+  } else {
+    if (req.body.password) {
+      req.body.password = md5(req.body.password);
     } else {
-      if (req.body.password) {
-        req.body.password = md5(req.body.password);
-      } else {
-        delete req.body.password;
-      }
-      await Account.updateOne({ _id: id }, req.body);
-      req.flash("success", "Cập nhật tài khoản thành công!");
+      delete req.body.password;
     }
-  } catch (error) {
-    console.log(error);
-    req.flash("error", "Cập nhật tài khoản thất bại!");
+    await Account.updateOne({ _id: id }, req.body);
+
+    req.flash("success", "Cập nhật tài khoản thành công");
   }
 
   res.redirect("back");
